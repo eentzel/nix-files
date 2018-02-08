c893430c0758cf204d19ab4de89888e3b924aa62
Eric Entzel <eric@ubermac.net>
2018-02-07 17:17:11 -0700

    Create user amanda

--- macbook-configuration.nix
+++ macbook-configuration.nix
@@ -87,6 +87,14 @@
     home = "/home/eric";
   };
 
+  users.extraUsers.amanda = {
+    isNormalUser = true;
+    uid = 1001;
+    description = "Amanda Entzel";
+    createHome =  true;
+    home = "/home/amanda";
+  };
+
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
